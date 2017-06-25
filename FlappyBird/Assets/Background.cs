cd9ecad08d73c8639316d47e347595ff82396958
Sung Chul Park <hopethink@naver.com>
2017-06-25 22:36:31 +0900

    - 움직이는 배경 완료 - 버드 기본 구현 Signed-off-by: Sung Chul Park <hopethink@naver.com>

---
+++ FlappyBird/Assets/Background.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour 
+{	
+	public SpriteRenderer left, right;
+	public Sprite spriteDay;
+	public Sprite spriteNight;
+	public GameObject pillarLeft, pillarRight;
+	public float speed, cut;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		transform.Translate (Vector3.left * Time.deltaTime * speed);
+		if (left.transform.position.x < cut) 
+		{
+			returnBackground ();
+		}
+		if (pillarLeft.transform.position.x < cut) 
+		{
+			returnPillar ();
+		}
+	}
+
+	void returnBackground()
+	{
+		SpriteRenderer temp = left;
+		left = right;
+		right = temp;
+		right.transform.localPosition = left.transform.localPosition + new Vector3(1.38f, 0, 0);
+		left.sortingOrder = 0;
+		right.sortingOrder = 1;
+	}
+
+	void returnPillar ()
+	{
+		GameObject temp = pillarLeft;
+		pillarLeft = pillarRight;
+		pillarRight = temp;
+		pillarRight.transform.localPosition = pillarLeft.transform.localPosition + new Vector3 (1.5f, 0, 0);
+		Vector3 newV = pillarRight.transform.localPosition;
+		newV.y = Random.Range (-0.5f, 0.5f);
+		pillarRight.transform.localPosition = newV;
+	}
+}
---
+++ FlappyBird/Assets/Bird.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour 
+{
+	public Rigidbody2D rigidBody;
+	public float jumpPower;
+	public float anumationSpeed;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void Jump()
+	{		
+		rigidBody.AddForce (Vector2.up * jumpPower, ForceMode2D.Impulse);
+	}
+
+	void FixedUpdate()
+	{
+		if ((-2 <= transform.position.y && transform.position.y <= 2) && Input.GetMouseButtonDown (0)) 
+			Jump ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Ceiling" || other.name == "Floor") 
+		{
+			Debug.Log (other.name);
+			rigidBody.velocity = Vector2.zero;
+		}
+	}
+}
