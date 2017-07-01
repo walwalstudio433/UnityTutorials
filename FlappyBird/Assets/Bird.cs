8fa927a93cadd0baa9e5ddf877f4ba9345ddc68e
Sung Chul Park <hopethink@naver.com>
2017-07-01 15:37:05 +0900

    [FlappyBird] Bird.cs Refactoring, Floor Sprite 구현, 충돌구현(회전 등)

--- FlappyBird/Assets/Bird.cs
+++ FlappyBird/Assets/Bird.cs
@@ -7,49 +7,78 @@ public class Bird : MonoBehaviour
 	public Background background;
 	public Rigidbody2D rigidBody;
 	public float jumpPower;
-	public float velocity;
-	public float rotateScale;
+	public float rotationSpeedForFlying;
+	public float rotationSpeedForDying;
 	public bool isCollided;
-	public bool jumped;
+	public bool isJumped;
+	public bool isDead;
+
+	public Quaternion deadRotation = Quaternion.Euler (0, 0, -90f);
 
 	void Update () 
 	{
-		if (Input.GetMouseButtonDown (0)) 
-			jumped = true;
-			
+		if (isDead)
+			return;
 
+		if (Input.GetMouseButtonDown (0) && !isCollided) 
+			isJumped = true;
+			
 		if (isCollided) {
-			transform.Rotate (0, 0, Time.deltaTime * rotateScale);
+			DyingAction ();
 		} else {
-			transform.localRotation = Quaternion.Euler ((new Vector3 (0, 0, rigidBody.velocity.y * rotateScale)));
+			FlyingAction ();
 		}
+	}
+
+	void DyingAction ()
+	{		
+		if (!Mathf.Approximately (transform.localRotation.z, deadRotation.z))
+			transform.localRotation = Quaternion.Slerp (transform.localRotation, deadRotation, Time.deltaTime * rotationSpeedForDying);
+	}
+
+	void FlyingAction ()
+	{
+		float rotationZofVelocity = rigidBody.velocity.y * rotationSpeedForFlying;
+		transform.localRotation = Quaternion.Euler (0, 0, rotationZofVelocity);
 	}
 
 	void FixedUpdate()
 	{		
-		if (jumped) {
+		if (isJumped) {
 			Jump ();
 		}
-
 	}
 
 	public void Jump()
 	{			
 		rigidBody.velocity = Vector2.zero;
 		rigidBody.AddForce (Vector2.up * jumpPower, ForceMode2D.Impulse);
-		jumped = false;
+		isJumped = false;
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		if (other.name == "Ceiling") {			
-			rigidBody.velocity = Vector2.zero;
-		} else if (other.name == "Floor") {
-			rigidBody.velocity = Vector2.zero;
-		} else if (other.name.StartsWith ("pillar")) {
-			//isCollided = true;
-			//rigidBody.velocity = Vector2.zero;
-			//background.Stop();
+		if (other.name == "Floor") {
+			CollidedByPillar ();
+			DeadByFloor ();
 		}
+
+		if (!isCollided && !isDead && other.name.StartsWith ("pillar")) {
+			CollidedByPillar ();
+		} 
+	}
+
+	void CollidedByPillar ()
+	{
+		isCollided = true;
+		rigidBody.velocity = Vector2.zero;
+		background.Stop ();
+	}
+
+	void DeadByFloor()
+	{
+		isDead = true;
+		transform.localRotation = deadRotation;
+		rigidBody.gravityScale = 0;
 	}
 }
