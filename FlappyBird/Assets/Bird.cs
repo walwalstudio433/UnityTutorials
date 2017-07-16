9edd42707b1f91cd789b1a9fda9fe5e87ba58028
tlu910 <tlu910@naver.com>
2017-07-17 00:10:29 +0900

    aaa

---
+++ FlappyBird/Assets/Bird.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKey)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 5, 0);
+        }
+
+        
+	}
+}
