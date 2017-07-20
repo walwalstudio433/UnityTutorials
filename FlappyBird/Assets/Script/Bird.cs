184f412bbfe0ae86b05aac975d47eefdc34e0754
tlu910 <tlu910@naver.com>
2017-07-20 23:20:10 +0900

    Bird, 장애물

---
+++ FlappyBird/Assets/Script/Bird.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("#: " + transform.rotation.z);
+        if (Input.GetButton("Jump"))
+        {
+            
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 5, 0);
+            if (transform.rotation.z < 0.08) { 
+                transform.Rotate(new Vector3(0,0,1.5f));
+            }
+        }
+        else if (transform.rotation.z > -0.03)
+        {
+            transform.Rotate(new Vector3(0, 0, -0.4f));
+        }
+
+
+
+
+    }
+}
---
+++ FlappyBird/Assets/Script/MakingHurdle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MakingHurdle : MonoBehaviour {
+    public  GameObject hurdle ;
+    public int velocity;
+    public int minY;
+    public int maxY;
+    GameObject instance;
+    
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("func", 1, 2);
+       }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  instace.transform.Translate(Vector3.left);
+        if (Input.GetButton("Cancel")) { 
+        CancelInvoke();
+     }
+    }
+
+    
+
+    void func()
+    {
+        Vector3 position = transform.localPosition;
+        position.y = Random.Range(minY, maxY);
+        instance = Instantiate(hurdle, position,transform.rotation);
+//        instance = Instantiate(hurdle, transform.localPosition, transform.rotation);
+        instance.GetComponent<Rigidbody2D>().AddForce(Vector2.left * velocity);
+    }
+
+
+}
